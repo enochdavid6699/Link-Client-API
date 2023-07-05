9f312c6a9c11b2d3cd56adccae4fc34dabbe44ac
Enoch <enochdavid6699@gmail.com>
2023-07-05 11:11:03 +0530

    Set up controller and routes

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const express = require('express');
+const router = express.Router();
+module.exports=router;
+
+//use exxpress router
+app.use( '/' , require('./routes/index' ));
+
 app.listen( port , function( err ){
     if(err){
         console.log('Error:' , err );
